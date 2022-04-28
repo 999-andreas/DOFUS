59dd068a2e5a03defa3dbcc7ad8f2476abdade74
Shizukin <104152230+Shizukin@users.noreply.github.com>
2022-04-28 17:25:26 +0200

    MENU FIN PARTIE 2
    VERSION MENU FIN DE PARTIE 2

--- main.c
+++ main.c
@@ -7,7 +7,11 @@ int main()
 
     BITMAP *image;
     BITMAP *dofus;
+    BITMAP *jouer;
     BITMAP *doubleBuffer;
+    BITMAP *revanche;
+    BITMAP *classement;
+    BITMAP *quitter;
     allegro_init();
     install_keyboard();
 
@@ -21,11 +25,19 @@ int main()
     doubleBuffer = create_bitmap(SCREEN_W, SCREEN_H);
     image = load_bitmap("minecraft.bmp",NULL);
     dofus = load_bitmap("dofus.bmp",NULL);
+    jouer = load_bitmap("jouer.bmp",NULL);
+    revanche = load_bitmap("revanche.bmp",NULL);
+    classement = load_bitmap("classement.bmp",NULL);
+    quitter = load_bitmap("quitter.bmp",NULL);
     while (!key[KEY_ESC])
     {
 
     clear_bitmap(doubleBuffer);
-    draw_sprite(image,dofus,200,15);
+    draw_sprite(image,dofus,200,0);
+    draw_sprite(image,jouer,255,175);
+    draw_sprite(image,revanche,255,258);
+    draw_sprite(image,classement,255,341);
+    draw_sprite(image,quitter,255,424);
     blit(image,doubleBuffer,0,0,0,0,SCREEN_W,SCREEN_H);
     blit(image,screen,0,0,0,0,SCREEN_W,SCREEN_H);
     }
