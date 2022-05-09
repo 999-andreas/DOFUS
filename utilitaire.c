acf7f5ed1fb2b7df161a99f2a5c268d4cf7e0a34
Tifenn007 <tifenn.james@edu.ece.fr>
2022-05-09 14:34:11 +0200

    correction utilitaire

--- utilitaire.c
+++ utilitaire.c
@@ -5,6 +5,7 @@ void setup_allegro()
 {
     allegro_init();
     install_keyboard();
+    install_mouse();
 
     set_color_depth(desktop_color_depth());
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,1300,700,0,0)!=0)
