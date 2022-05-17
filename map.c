ec51b24a0b95a4cdd1c56e2c020590fa7ec0647e
999-andreas <andreas.chatel5@gmail.com>
2022-05-17 14:42:24 +0200

    affichage CAC

--- map.c
+++ map.c
@@ -303,23 +303,29 @@ void affiche_selectSORT(BITMAP*buffer, BITMAP*jaune, int etat_hotbar[9])
 void attaque_CAC(t_joueur *michel, int joueurTour,BITMAP*orange,BITMAP*buffer, int nb_joueur)
 {
     int i;
-    int j;
-    int k;
+    int j=0;
+    int k=0;
 
+    int debutx = ((michel[joueurTour].posx)-50)/50;
+    int finx = ((michel[joueurTour].posx)+100)/50;
+
+    int debuty = ((michel[joueurTour].posy)-50)/50;
+    int finy = ((michel[joueurTour].posy)+150)/50;
 
     for (i=0; i<nb_joueur; i++)
     {
-        for(j = ((michel[joueurTour].posx)-50); j <((michel[joueurTour].posx)+50); j+=50)
+        for(j = (debutx); j <(finx); j++)
         {
-            printf("test1 %d\n", j);
 
-            for(k = ((michel[joueurTour].posy)+50); k <((michel[joueurTour].posy)+100); k+=50)
+            for(k = (debuty); k <(finy); k++)
             {
-                printf("test2\n");
-                if(michel[i].posx == j && michel[i].posy == k)
+
+                if(michel[i].posx == j*50 && michel[i].posy == k*50)
                 {
-                    printf("test3\n");
-                    blit(orange, buffer, 0,0 ,i,j, 50,50);
+                    if((michel[joueurTour].posx == j*50) && (michel[joueurTour].posy == k*50))
+                        continue;
+
+                    blit(orange, buffer, 0,0 ,j*50,k*50, 50,50);
                 }
             }
         }
